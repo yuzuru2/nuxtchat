88aa19a96f8f081485b609e2dfc11c6c1da43ed2
yuzuru2 <62276337+yuzuru2@users.noreply.github.com>
2020-05-05 23:48:35 +0900

    Update room.ts

--- nodejs/front/file/store/room.ts
+++ nodejs/front/file/store/room.ts
@@ -1,4 +1,7 @@
+import { Howl } from 'howler';
 import { Mutation, VuexModule, Module } from 'vuex-module-decorators';
+import { constant } from '~/constant';
+import { RoomStore } from '~/store';
 
 export interface i_room {
   userId: string;
@@ -25,6 +28,10 @@ export interface i_room {
   }[];
 
   jwt_token: string;
+
+  title: string;
+
+  sound_icon_opacity: number;
 }
 
 @Module({ stateFactory: true, namespaced: true, name: 'room' })
@@ -53,6 +60,10 @@ export default class Room extends VuexModule implements i_room {
 
   jwt_token: string = '';
 
+  title: string = constant.TITLE;
+
+  sound_icon_opacity: number = 0.3;
+
   @Mutation
   set_userId(userId: string) {
     this.userId = userId;
@@ -71,7 +82,43 @@ export default class Room extends VuexModule implements i_room {
       createdAt: Date;
     }[]
   ) {
+    const tmp = this.talk_list;
     this.talk_list = talk_list;
+
+    if (process.client && JSON.stringify(tmp) !== JSON.stringify(talk_list)) {
+      const ring_sound = () => {
+        if (this.sound_icon_opacity === 1) {
+          const sound = new Howl({
+            src: ['sound.mp3'],
+          });
+          sound.play();
+        }
+      };
+
+      if (tmp.length === 0 && talk_list.length > 0) {
+        this.title = '新着メッセージ';
+        setTimeout(() => {
+          RoomStore.set_title(constant.TITLE);
+        }, 1500);
+
+        ring_sound();
+        return;
+      }
+
+      if (
+        tmp.length > 0 &&
+        talk_list.length > 0 &&
+        tmp[0].talkId !== talk_list[0].talkId
+      ) {
+        this.title = '新着メッセージ';
+        setTimeout(() => {
+          RoomStore.set_title(constant.TITLE);
+        }, 1500);
+
+        ring_sound();
+        return;
+      }
+    }
   }
 
   @Mutation
@@ -92,5 +139,15 @@ export default class Room extends VuexModule implements i_room {
   @Mutation
   set_jwt_token(jwt_token: string) {
     this.jwt_token = jwt_token;
+  }
+
+  @Mutation
+  set_title(title: string) {
+    this.title = title;
+  }
+
+  @Mutation
+  set_sound_icon_opacity() {
+    this.sound_icon_opacity = this.sound_icon_opacity === 1 ? 0.3 : 1;
   }
 }
